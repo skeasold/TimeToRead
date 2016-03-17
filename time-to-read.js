1348dd647eac512e40108b4ab3d47dc60481e30c
Skylar King <sking@crazyawesome.io>
2016-03-16 19:50:40 -0500

    Add floating word count with minimal styling. Put js inside function.

--- time-to-read.js
+++ time-to-read.js
@@ -1,3 +1,7 @@
-var count = document.getElementById('time-to-read-view').innerHTML.split(' ').length;
-var wpm = Math.round(count / 230);
-console.log(wpm + " minutes.")
+function timeToRead() {
+  var count = document.getElementById('time-to-read-view').innerHTML.split(' ').length;
+  var wpm = Math.round(count / 230);
+  document.getElementById("float").innerHTML = wpm + " minutes.";
+}
+
+timeToRead();
