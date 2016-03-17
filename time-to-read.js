368f9c9fa061cf2618dd71b1cd0d423472606752
Skylar King <sking@crazyawesome.io>
2016-03-16 20:09:34 -0500

    Add float div creation from js. Seperated into two functions. Needs styling.

--- time-to-read.js
+++ time-to-read.js
@@ -1,7 +1,13 @@
 function timeToRead() {
   var count = document.getElementById('time-to-read-view').innerHTML.split(' ').length;
-  var wpm = Math.round(count / 230);
+  wpm = Math.round(count / 230);
+}
+
+function showTime() {
+  var body = document.getElementById("body").innerHTML;
+  document.getElementById("body").innerHTML = '<div class="float" id="float"></div>' + body;
   document.getElementById("float").innerHTML = wpm + " minutes.";
 }
 
 timeToRead();
+showTime();
